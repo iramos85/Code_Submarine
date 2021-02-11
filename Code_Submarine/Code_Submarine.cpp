931a985f5b34514ed1db1871dfd489d9eb93962e
iramos85 <juventuddisponible@gmail.com>
2021-02-11 09:16:19 -0600

    added initial game logic

--- Code_Submarine/Code_Submarine.cpp
+++ Code_Submarine/Code_Submarine.cpp
@@ -1,20 +1,21 @@
-// Code_Submarine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Game premise and instructions
+    std::cout << "You're attempting to sabotage the engine of a Nuclear Nazi sub." << "\n";
+    std::cout << "You must enter the correct code sequence to flood the first engine compartment!" << "\n";
+
+    //declare 3 number code
+    int a = 4;
+    int b = 8;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << "\n";
+    std::cout << product << "\n";
+
+    system("PAUSE");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
